2a72e9dcf86b97dcdfcb50dd395a162e96ee63fd
Victor Roma <66295991+RomaNFS21@users.noreply.github.com>
2024-12-13 20:22:46 -0300

    Criação do ambiente

---
+++ hook-activities/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React, { useState } from "react";
+import CountClicks from "./Components/CountClicks";
+
+function App() {
+  return (
+    <div className="App">
+      <CountClicks />
+    </div>
+  );
+}
+
+export default App;
---
+++ hook-activities/src/Components/CountClicks.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function CountClicks() {
+  const [count, setCount] = useState(0);
+
+  if (setCount < 0) {
+    setCount = 0;
+  }
+
+  return (
+    <div>
+      <p>The current count is: {count}</p>
+      <button onClick={() => setCount(count + 1)}> Increase count </button>
+      <button onClick={() => setCount(count - 1)}> Decrease count </button>
+    </div>
+  );
+}
+
+export default CountClicks;
